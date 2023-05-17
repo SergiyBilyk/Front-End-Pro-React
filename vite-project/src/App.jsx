b144dee7c7971646020d3532151b3c7326d57e28
SergiyBilyk <shivantom7@gmail.com>
2023-05-17 20:56:52 +0300

    hw-28

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import {Button} from './components/Buttons/Button'
+import './App.css'
+
+// function App() {
+//   return(<h1>app</h1>)
+// }
+
+function App() {
+  return (<div>
+    <h2>Add and remove buttons</h2>
+    <Button name = 'Add'></Button>
+    <Button name = 'Remove'></Button>
+  </div>)
+}
+
+export default App
---
+++ vite-project/src/components/Buttons/Button.jsx
@@ -0,0 +1,4 @@
+import './Button.css'
+
+export const Button = (props) => 
+<button className="btn">{props.name}</button>
