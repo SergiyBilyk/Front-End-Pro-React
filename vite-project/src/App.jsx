fe2f51c8f3430d5a42e549c27eb6095cf074a182
SergiyBilyk <shivantom7@gmail.com>
2023-06-28 13:19:19 +0300

    react hook form

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,47 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement, reset } from "./redux/counterSlice";
-import { login, logout } from "./redux/authSlice";
 import "./App.css";
-import { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  const dispatch = useDispatch();
-
-  const { value } = useSelector((store) => store.counter);
-
-  const handleIncrement = () => {
-    dispatch(increment(number));
-  };
-
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
-
-  const handleReset = () => {
-    dispatch(reset());
-  };
-
-  const { isAuth } = useSelector((store) => store.auth);
 
   return (
     <div className="container">
-      {isAuth ? (
-        <button onClick={() => dispatch(logout())}>Logout</button>
-      ) : (
-        <button onClick={() => dispatch(login())}>Login</button>
-      )}
-      Hello
-      <input
-        type="text"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-      />
-      <div>Counter: {value}</div>
-      <button onClick={handleIncrement}>increment</button>
-      <button onClick={handleDecrement}>decrement</button>
-      <button onClick={handleReset}>reset</button>
+      <Form />
     </div>
   );
 }
