3763263d29b6a650283e5333ef800b73152a2cf0
SergiyBilyk <shivantom7@gmail.com>
2023-08-15 17:16:43 +0300

    css style modules

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {useState} from 'react'
 import "./Post.css"
+import style from './Post.module.css'
 
 const Post = ({ userId, id, title, body, isLoading }) => {
 
   return (
     <div className="wrapper">
-      <div className="block">
+      <div className={style.blockRed}>
         <h3>{id}</h3>
         <h2>{title}</h2>
         <div>{body}</div>
